797463fb1f53b075b3af8bbe8e4f7874172d73e9
raulng9 <salprez4@gmail.com>
2021-04-27 12:36:32 +0200

    SDI-2021-811-SW-11.1-Servicio SOAP

---
+++ sdi2021-811-lab-soap-sw/src/test/java/com/uniovi/Sdi2021811LabSoapSwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2021811LabSoapSwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
